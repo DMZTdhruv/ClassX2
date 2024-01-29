084fa4d2b6154b53d83bf4d344eca082da65ac91
DMZTdhruv <shruvparmar.gmail.com>
2024-01-29 23:34:28 +0530

    Added userSignIn controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -19,3 +19,18 @@ export const userSignUpController = async (req,res) => {
     res.status(500).json({message: err.message})
   }
 }
+
+export const userSingInController = async (req,res) => {
+  try {
+    const {email,password} = req.body;
+    const userToFind = await User.findOne({email: email})
+    if(!userToFind) return res.status(404).json({message: "User not found"})
+    
+    const passwordMatch = bcrypt.compareSync(password,userToFind.password)
+    
+    if(passwordMatch) return res.status(200).json({message: "Successful logged in", user: {email: userToFind.email, userId: userToFind._id}});
+    res.status(401).json({message: "Invalid credentials"})
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  } 
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,9 @@
 import express from "express"
-import { userSignUpController } from "../controllers/auth.js"
+import { userSignUpController, userSingInController } from "../controllers/auth.js"
 
 const router = express.Router()
 
 router.post("/singUp", userSignUpController)
-router.get("/test" , (req,res) => {
-  res.status(200).json({message: "Hello"})
-})
+router.post("/singIn", userSingInController)
 
 export default router;

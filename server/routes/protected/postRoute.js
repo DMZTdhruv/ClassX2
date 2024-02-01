6a35df3c0803d0512400915723803fe72be852ec
DMZTdhruv <shruvparmar.gmail.com>
2024-02-01 18:40:11 +0530

    Added get post

--- server/routes/protected/postRoute.js
+++ server/routes/protected/postRoute.js
@@ -2,6 +2,9 @@
 import express from "express";
 import { authenticateUserToken } from "../../middlewares/authMiddleware.js";
 import { createPostController } from "../../controllers/post/createPostController.js";
+import { paginatedResults } from "../../middlewares/paginatedResults.js";
+import Post from "../../models/post/post.model.js";
+import { getPostController } from "../../controllers/post/getPostController.js";
 
 const router = express.Router();
 
@@ -10,5 +13,12 @@ router.post(
   authenticateUserToken,
   createPostController
 );
+
+router.get(
+  "/get-post",
+  authenticateUserToken,
+  paginatedResults(Post),
+  getPostController
+)
 
 export default router;

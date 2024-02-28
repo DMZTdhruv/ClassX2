556ef2b9b7bd27899be6112e320c14af60a6448f
DMZTdhruv <shruvparmar.gmail.com>
2024-02-28 15:39:24 +0530

    Fixed import bug

--- server/services/ProfileService/followUserService.js
+++ server/services/ProfileService/followUserService.js
@@ -1,5 +1,5 @@
 import UserProfileRepository from '../../repositories/UserProfileRepository.js'
-import {validateFollow} from '../../validations/FollowUserValidator.js'
+import { validateFollow } from '../../validations/FollowUserValidator.js'
 
 export default async function followUserService(userId, userToFollowId) {
   try {
@@ -7,20 +7,19 @@ export default async function followUserService(userId, userToFollowId) {
 
     const userRepo = new UserProfileRepository()
 
-
     //checking users
     checkUsers(userId, userToFollowId)
 
     // checking if the current user is already following
-    const isAlreadyFollowingEachOther = await userRepo.checkUserFollowStatus(userId,userToFollowId);
+    const isAlreadyFollowingEachOther = await userRepo.checkUserFollowStatus(userId, userToFollowId)
 
-    if(isAlreadyFollowingEachOther){
-      throw new Error("Already following")
+    if (isAlreadyFollowingEachOther) {
+      throw new Error('Already following')
     }
 
     await userRepo.followUser(userId, userToFollowId)
     return {
-      message: `${userId} is now following ${userToFollowId}`
+      message: `${userId} is now following ${userToFollowId}`,
     }
   } catch (err) {
     throw new Error(err.message)
